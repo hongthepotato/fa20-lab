18faf441c2428bea7fb79d4915e7ec28c3d8b87a
hshen <gun1714133422@gmail.com>
2024-10-31 14:08:21 +0800

    lab01 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,27 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    node *turtle = head;
+    node *hare = head;
+    //check for null input
+    if (turtle == NULL) {
+        return 0;
+    }
+    hare = (hare->next)->next;
+    if (hare == NULL) {
+        return 0;
+    }
+    while (hare != turtle) {
+        hare = (hare->next);
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = (hare->next);
+        if (hare == NULL) {
+            return 0;
+        }
+        turtle = turtle->next;
+    }
+
+    return 1;
 }
